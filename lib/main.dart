1ef86cfe9ab22a3a33052785c9d604bd30687776
Luca <luca.dommel@live.de>
2023-05-05 16:18:26 +0200

    tested FromFirebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_project_2023/widgets/pages/group_page.dart';
 import 'package:flutter_project_2023/firebase_options.dart';
 import 'package:flutter_project_2023/widgets/pages/settings_page.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter_project_2023/widgets/pages/shopping_list_page.dart';
 
--- lib/repositories/group_repository.dart
+++ lib/repositories/group_repository.dart
@@ -1,15 +1,37 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_project_2023/repositories/group_model.dart';
 import 'package:flutter_project_2023/repositories/groups_model.dart';
 
 class GroupRepository {
+  final firestore = FirebaseFirestore.instance;
+  List<Group> groupsList = [];
+
   Future<Groups> getGroups() async {
-    var groups = Groups([
+
+    var groups2 = firestore.collection("/groups");
+
+    await groups2.get()
+    .then((querySnapshot) {
+      debugPrint("##############");
+        for (var docSnapshot in querySnapshot.docs) {
+          
+          final data = docSnapshot.data();
+
+          for (var test in data.values) {
+            groupsList.add(test);
+          }
+        }
+      },
+      onError: (e) => debugPrint("Error completing: $e")
+    );
+
+    return Groups([
       Group(1, "group#1", "Familie HWR", null),
       Group(2, "group#2", "Familie Flutter", "Flutter Gruppe")
     ]);
 
-    return groups;
   }
 }
